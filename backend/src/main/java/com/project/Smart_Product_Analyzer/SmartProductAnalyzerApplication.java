ed8d4fd625bade514b544f6d3259e21e8e408ac4
Saucyyy8 <mailsuhas.madhu@gmail.com>
2025-10-03 19:40:48 +0530

    EnableCaching

--- backend/src/main/java/com/project/Smart_Product_Analyzer/SmartProductAnalyzerApplication.java
+++ backend/src/main/java/com/project/Smart_Product_Analyzer/SmartProductAnalyzerApplication.java
@@ -2,7 +2,9 @@ package com.project.Smart_Product_Analyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SmartProductAnalyzerApplication {
 
